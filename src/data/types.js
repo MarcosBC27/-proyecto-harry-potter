096cc92078fcf2c862dcd3114b297dc01a9b54ce
Marcos <marcos.ben.cont@gmail.com>
2021-10-23 14:58:27 -0500

    Ajustes finales para publicacion

--- src/data/types.js
+++ src/data/types.js
@@ -1,2 +1,2 @@
 export const UPDATE_FAVOURITE = 'UPDATE_FAVOURITE';
-export const urlApi = 'https://my-json-server.typicode.com/MarcosBC27/proyecto-harry-potter-api/';
+export const urlApi = 'https://api-harry.herokuapp.com/';
